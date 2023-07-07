91629adf9e75b24771156b0a517cdac3af934bbf
steffanhalv <steffanhalv@gmail.com>
2023-07-07 19:32:19 +0000

    Build: (8b49b69) Merge pull request #354 from vueplayground/next
    Fix spacing

---
+++ assets/javascript-797bf064.js
@@ -0,0 +1,6 @@
+import{conf as t,language as e}from"./typescript-9a00e66c.js";import"./Design-944ba743.js";import"./SectionRenderer-ac58a36d.js";import"./compiler-6005b5aa.js";import"./index-f8936b82.js";import"./ModalProfileSubscriptionPricing-82faa37c.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.39.0(ff3621a3fa6389873be5412d17554294ea1b0941)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var c=t,l={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{c as conf,l as language};
