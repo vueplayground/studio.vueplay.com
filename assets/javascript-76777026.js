0a99fa68a8426de699f0b41379a6f2cda143f142
steffanhalv <steffanhalv@gmail.com>
2023-07-17 08:25:51 +0000

    Build: (d94e483) Merge pull request #386 from vueplayground/next
    Next

--- assets/javascript-76777026.js
+++
@@ -1,6 +0,0 @@
-import{conf as t,language as e}from"./typescript-6eb55d89.js";import"./Design-de591e1a.js";import"./SectionRenderer-b91b139f.js";import"./compiler-e2eb3cc6.js";import"./index-6bf84393.js";import"./ModalProfileSubscriptionPricing-8c4bde46.js";/*!-----------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.39.0(ff3621a3fa6389873be5412d17554294ea1b0941)
- * Released under the MIT license
- * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
- *-----------------------------------------------------------------------------*/var c=t,l={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{c as conf,l as language};
