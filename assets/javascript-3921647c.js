c1ef017ce6528c60ea8f1b8c187915f0456ed45a
steffanhalv <steffanhalv@gmail.com>
2023-08-09 09:36:38 +0000

    Build: (3b3b26d) Merge pull request #399 from vueplayground/next
    Fix background color

---
+++ assets/javascript-3921647c.js
@@ -0,0 +1,6 @@
+import{conf as t,language as e}from"./typescript-f7d873c9.js";import"./Design-04ab51c4.js";import"./SectionRenderer-3b96bf39.js";import"./compiler-9127435b.js";import"./index-a722ddaa.js";import"./ModalProfileSubscriptionPricing-e1472041.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.39.0(ff3621a3fa6389873be5412d17554294ea1b0941)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var c=t,l={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{c as conf,l as language};
