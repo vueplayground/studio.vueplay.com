2e97180b34e82f29ba2fab2ee3bba943fa75fdf0
steffanhalv <steffanhalv@gmail.com>
2023-08-09 08:40:06 +0000

    Build: (b89738c) Merge pull request #396 from vueplayground/next
    Add suspense icon

---
+++ assets/javascript-eed82ec4.js
@@ -0,0 +1,6 @@
+import{conf as t,language as e}from"./typescript-0e8857bc.js";import"./Design-b9bbe037.js";import"./SectionRenderer-74c3f93a.js";import"./compiler-fd5c94b6.js";import"./index-b9689596.js";import"./ModalProfileSubscriptionPricing-212dd6ca.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.39.0(ff3621a3fa6389873be5412d17554294ea1b0941)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var c=t,l={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{c as conf,l as language};
