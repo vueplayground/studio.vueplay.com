beb4b4bcd253f7626e79fdd7a193c7cd93505a0b
steffanhalv <steffanhalv@gmail.com>
2023-08-14 13:39:14 +0000

    Build: (c2208b1) Merge pull request #404 from vueplayground/next
    Fix font size

--- assets/javascript-d40879f3.js
+++
@@ -1,6 +0,0 @@
-import{conf as t,language as e}from"./typescript-e136404c.js";import"./Design-570b0bc1.js";import"./SectionRenderer-839d6d93.js";import"./compiler-14e05cd6.js";import"./index-d2c1754f.js";import"./ModalProfileSubscriptionPricing-3cff02b6.js";/*!-----------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.39.0(ff3621a3fa6389873be5412d17554294ea1b0941)
- * Released under the MIT license
- * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
- *-----------------------------------------------------------------------------*/var c=t,l={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{c as conf,l as language};
