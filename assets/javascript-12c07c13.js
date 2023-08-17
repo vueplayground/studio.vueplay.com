f36b142f1ae1c12c5a74f445da78f1780df56208
steffanhalv <steffanhalv@gmail.com>
2023-08-17 19:28:46 +0000

    Build: (b1478b7) Merge pull request #413 from vueplayground/next
    Add check for tailwind

--- assets/javascript-12c07c13.js
+++
@@ -1,6 +0,0 @@
-import{conf as t,language as e}from"./typescript-41a9a7b8.js";import"./Design-6f8b6c60.js";import"./SectionRenderer-67d16d96.js";import"./compiler-c89f8a9b.js";import"./index-10a343f6.js";import"./ModalProfileSubscriptionPricing-9525f67b.js";/*!-----------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.39.0(ff3621a3fa6389873be5412d17554294ea1b0941)
- * Released under the MIT license
- * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
- *-----------------------------------------------------------------------------*/var c=t,l={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{c as conf,l as language};
