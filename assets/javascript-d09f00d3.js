bea9162e6e046e44709e4b655f5b746a96bb3d3c
steffanhalv <steffanhalv@gmail.com>
2023-08-17 21:55:46 +0000

    Build: (e2136b7) Merge pull request #417 from vueplayground/next
    Label icons

---
+++ assets/javascript-d09f00d3.js
@@ -0,0 +1,6 @@
+import{conf as t,language as e}from"./typescript-62280d81.js";import"./Design-f50cc7ea.js";import"./SectionRenderer-b7f5021e.js";import"./compiler-2e87a593.js";import"./index-b1d75112.js";import"./ModalProfileSubscriptionPricing-b30e80d1.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.39.0(ff3621a3fa6389873be5412d17554294ea1b0941)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var c=t,l={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{c as conf,l as language};
