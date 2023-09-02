5e85c8da8165fb6ec969b704dda051d44f74dbd1
steffanhalv <steffanhalv@gmail.com>
2023-09-02 16:26:03 +0000

    Build: (def8cb9) Merge pull request #438 from vueplayground/next
    Fix html length

---
+++ assets/javascript-71ae9a02.js
@@ -0,0 +1,6 @@
+import{conf as t,language as e}from"./typescript-39a3c387.js";import"./Design-0169b044.js";import"./SectionRenderer-507a8b0d.js";import"./compiler-4fa02ebc.js";import"./index-f2017e4b.js";import"./ModalProfileSubscriptionPricing-f3c0d7b4.js";/*!-----------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Version: 0.39.0(ff3621a3fa6389873be5412d17554294ea1b0941)
+ * Released under the MIT license
+ * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
+ *-----------------------------------------------------------------------------*/var c=t,l={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","static","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{c as conf,l as language};
