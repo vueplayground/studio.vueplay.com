4f345ade76b51ac934bb652a91bdad5eed660cd5
steffanhalv <steffanhalv@gmail.com>
2023-10-17 07:40:49 +0000

    Build: (d53bbb6) Merge pull request #477 from vueplayground/next
    Fix path & async methods

---
+++ assets/Preview-f3e92e5b.js
@@ -0,0 +1,6 @@
+import{S as r}from"./SectionRenderer-ec7924c4.js";import{_ as n,r as o,c as p,j as l,o as m}from"./index-26b8544c.js";import"./compiler-983d30f0.js";const c={components:{SectionRenderer:r},inject:["compiler"],data:()=>({ast:null,main:""}),watch:{main(e){e.indexOf("<template><")!==-1&&(e=e.replace("<template><",`<template>
+        <`)),this.ast=this.compiler.parse(e,!0,!0)}},async mounted(){var a,i;let e=((a=window==null?void 0:window.location)==null?void 0:a.hash)||((i=window==null?void 0:window.frameElement)==null?void 0:i.getAttribute("uid"));if(e){e=e.replace("#","");let t=await fetch(`https://api.vueplay.io/types/sfc/${e}?ref=HEAD&type=vue`);this.main=await t.text(),this.ast=this.compiler.parse(this.main,!0,!0)}else this.main=`
+        <template>
+          <div>No Source</div>
+        </template>
+      `,this.ast=this.compiler.parse(this.main,!0,!0);window.addEventListener("message",t=>{typeof t.data.raw<"u"&&(this.main=t.data.raw,this.ast=this.compiler.parse(this.main,!0,!0))})}},d={class:"preview"};function u(e,a,i,t,_,h){const s=o("SectionRenderer");return m(),p("div",d,[l(s,{ast:e.ast,main:e.main,sidemenu:"0px",mobile:!1,fs:{},events:!0,full:!0,lazy:!0,tailwind:!0},null,8,["ast","main"])])}const y=n(c,[["render",u],["__scopeId","data-v-47948973"]]);export{y as default};
