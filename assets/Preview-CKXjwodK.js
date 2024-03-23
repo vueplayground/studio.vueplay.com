932e3e4543376b4ed2f50941d1b8b18e7e87cb10
steffanhalv <steffanhalv@gmail.com>
2024-03-23 15:06:16 +0000

    Build: (366b817) Merge pull request #622 from vueplayground/next
    Add github workflow permission

--- assets/Preview-CKXjwodK.js
+++
@@ -1,6 +0,0 @@
-import{S as r}from"./SectionRenderer-B09LcrJP.js";import{_ as n,c as o,j as p,r as m,o as l}from"./index-BR9zGSjc.js";import"./compiler-CVZJo_qz.js";const c={components:{SectionRenderer:r},inject:["compiler"],data:()=>({ast:null,main:""}),watch:{main(e){e.indexOf("<template><")!==-1&&(e=e.replace("<template><",`<template>
-        <`)),this.ast=this.compiler.parse(e,!0,!0)}},async mounted(){var a,i;let e=((a=window==null?void 0:window.location)==null?void 0:a.hash)||((i=window==null?void 0:window.frameElement)==null?void 0:i.getAttribute("uid"));if(e){e=e.replace("#","");let t=await fetch(`https://api.vueplay.io/types/sfc/${e}?ref=HEAD&type=vue`);this.main=await t.text(),this.ast=this.compiler.parse(this.main,!0,!0)}else this.main=`
-        <template>
-          <div>No Source</div>
-        </template>
-      `,this.ast=this.compiler.parse(this.main,!0,!0);window.addEventListener("message",t=>{typeof t.data.raw<"u"&&(this.main=t.data.raw,this.ast=this.compiler.parse(this.main,!0,!0))})}},d={class:"preview"};function u(e,a,i,t,h,f){const s=m("SectionRenderer");return l(),o("div",d,[p(s,{ast:e.ast,main:e.main,sidemenu:"0px",mobile:!1,fs:{},events:!0,full:!0,lazy:!0,tailwind:!0},null,8,["ast","main"])])}const y=n(c,[["render",u],["__scopeId","data-v-47948973"]]);export{y as default};
